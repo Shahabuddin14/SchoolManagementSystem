ef0935303fe5c3e24eb07a85d2d97274a4f259c0
Shahabuddin14 <shahabuddin650@gmail.com>
2020-04-16 00:06:06 +0600

    Basic School Management System

---
+++ includes/side_bar.php
@@ -0,0 +1,21 @@
+<div class="span3">
+    <div class="sidebar">
+        <ul class="widget widget-menu unstyled">
+            <li class="active">
+                <a href="home.php"><i class="menu-icon icon-dashboard"></i>Dashboard </a>
+            </li>
+            <li>
+                <a href="profile.php"><i class="menu-icon icon-bullhorn"></i>Profile </a>
+            </li>
+        </ul>
+        <!--/.widget-nav-->
+        <ul class="widget widget-menu unstyled">
+            <li><a href="pay.php"><i class="menu-icon icon-bold"></i>Payment </a></li>
+        </ul>
+        <!--/.widget-nav-->
+        <ul class="widget widget-menu unstyled">
+            <li><a href="logout.php"><i class="menu-icon icon-signout"></i>Logout </a></li>
+        </ul>
+    </div>
+    <!--/.sidebar-->
+</div>
